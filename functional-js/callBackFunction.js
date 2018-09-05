e83ce01dd5cee6fdd9d41eb22968a8b794b867ff
Aminulislamsabbir <aminulislam.sabbir11@gmail.com>
2018-09-05 19:17:58 +0600

    callback

---
+++ functional-js/callBackFunction.js
@@ -0,0 +1,14 @@
+// When I pass a Function inside a function and pass the function to argument form it is call back function
+
+function mycall (name,cp){
+    cp (name);
+} 
+
+mycall('Aminul Islam Sabbir', function (name){
+    console.log(name.toUpperCase());
+})
+
+mycall('Adol Rashid', function (name) {
+    console.log('Hello ' + name.toLowerCase())
+})
+
