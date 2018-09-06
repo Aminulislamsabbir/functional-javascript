4a883c2f0c1aadd98dfb28bceeeaa1b6993aedb8
Aminulislamsabbir <aminulislam.sabbir11@gmail.com>
2018-09-07 01:22:01 +0600

    callbackfunction

--- functional-js/callBackFunction.js
+++ functional-js/callBackFunction.js
@@ -12,3 +12,54 @@ mycall('Adol Rashid', function (name) {
     console.log('Hello ' + name.toLowerCase())
 })
 
+function callPrint (name, cp1, cp2) {
+    console.log(name);
+    
+    cp1(name);
+    cp2(name);
+}
+
+callPrint ('Aminul Islam Sabbir is a problem solver' , function (name) {
+    console.log(name.toUpperCase());
+}, function (name){
+    console.log(name.split(''))
+})
+
+var obj = {
+    Name : 'Aminul Islam Sabbir',
+    Age :18,
+    Roll : 773309,
+    Skill : ['JavaScript', 'Hltm', 'CSS', 'Java'],
+    Passion :['Wild Photograper','World Tour','Problem Solveing']
+}
+
+function Greet (data,cp) {
+    cp(data) ;
+}
+
+Greet(obj, function(data){
+    for (var props in data) {
+        console.log('AI Sabbir ' + props);
+    }
+})
+
+Greet(obj, function (data) {
+        if(obj.Age > 20) {
+            console.log('You are abult');
+            
+        }else {
+            console.log('You are not abult ');
+            
+        }
+})
+
+Greet(obj, function (data) {
+    for(var props in data) {
+        console.log('AI Sabbir ' + data[props]);
+    }
+})
+
+
+Greet (obj, function (data) {
+    console.log(obj.Name.toUpperCase());
+})
