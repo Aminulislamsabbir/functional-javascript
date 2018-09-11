33c54ba9c9d0a4873bd582ebf39f3e8633f6152e
Aminulislamsabbir <aminulislam.sabbir11@gmail.com>
2018-09-12 03:56:44 +0600

    Foreach

---
+++ functional-js/Foreach.js
@@ -0,0 +1,20 @@
+var arr = [1,2,3,4,5,6,7,8,9]
+
+arr.forEach(function(element, index) {
+    console.log('Element Value is ' + element + ' Index of ' + index);
+})
+
+// foreach function make
+
+function Myfunction (arr, callback){
+    for(var i = 0; i < arr.length; i++) {
+        callback(arr[i] ,i, arr)
+    }
+} 
+
+Myfunction(arr , function(element, i, arr){
+    console.log('Element value is ' + element + ' Index of ' + i + ' and Array ' + arr);
+    
+})
+
+
